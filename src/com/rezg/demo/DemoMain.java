cb7b0acc0630f3964026eb3d0411931921dd0709
kajan-rezg <varatharaja.kajamugan@rezgateway.com>
2016-06-12 12:07:41 +0530

    jira-id-3 conflicts resolved

--- src/com/rezg/demo/DemoMain.java
+++ src/com/rezg/demo/DemoMain.java
@@ -5,6 +5,7 @@ public class DemoMain {
 	public static void main(String[] args) {
 		System.out.println("This is for develop branch");
 		System.out.println("jira id 1 added");
+		System.out.println("jira id 2 added");
 		System.out.println("jira id 3 added");
 	}
 	
