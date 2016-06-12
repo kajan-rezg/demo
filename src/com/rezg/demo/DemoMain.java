f4d278e3287851cdd835ee40cbbaca0a7de79b8c
kajan-rezg <varatharaja.kajamugan@rezgateway.com>
2016-06-12 11:09:59 +0530

    develop branch created

--- src/com/rezg/demo/DemoMain.java
+++ src/com/rezg/demo/DemoMain.java
@@ -3,7 +3,7 @@ package com.rezg.demo;
 public class DemoMain {
 
 	public static void main(String[] args) {
-		System.out.println("This is for master branch");
+		System.out.println("This is for develop branch");
 	}
 
 }
